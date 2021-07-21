f3b02b01d2af58c8148d73f65b3a93be50b989ae
Jacob Felknor <jacobfelknor073@gmail.com>
2021-07-21 10:49:05 -0600

    pass selected Car to view data

--- src/app/view-data/view-data.component.ts
+++ src/app/view-data/view-data.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Car } from '../pick-car/car';
 
 @Component({
   selector: 'app-view-data',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewDataComponent implements OnInit {
 
+  @Input() public selectedCar: Car | undefined;
+
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
